81b8e46f31d5b3effc991e4eab188f900b67cbec
k391160334 <maseqqqq@gmail.com>
2024-05-09 02:34:48 +0900

    swagger 문서 생성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
+@ApiTags('Hello World! ')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'get Hello World',
+    description: 'get Simple Hello World',
+  })
+  @ApiCreatedResponse({
+    description: 'return Simple Hello World',
+  })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './util/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 스웨거 설정
+  setupSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
