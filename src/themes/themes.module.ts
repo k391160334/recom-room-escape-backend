c884c1565e385541c0155eabcfbaceaedb920c90
k391160334 <maseqqqq@gmail.com>
2024-05-26 23:55:39 +0900

    [PRJ-1] theme module에 CafeLocation 엔티티 의존성 주입

--- src/themes/themes.module.ts
+++ src/themes/themes.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cafe } from 'src/cafes/cafe.entity';
+import { ThemeTime } from 'src/themeTimes/themeTime.entity';
+import { CafeLocation } from 'src/cafeLocations/cafeLocation.entity';
 import { ThemesService } from './themes.service';
 import { ThemesController } from './themes.controller';
 import { Theme } from './theme.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Theme, Cafe])],
+  imports: [TypeOrmModule.forFeature([Theme, Cafe, ThemeTime, CafeLocation])],
   providers: [ThemesService],
   controllers: [ThemesController],
 })
